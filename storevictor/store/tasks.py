52cbf22c221767c1f992bb695967011ed81a091a
Elliot Menkah <elliotsmenkah@gmail.com>
2021-10-25 09:32:18 +0000

    Updated Requirements

--- storevictor/store/tasks.py
+++ storevictor/store/tasks.py
@@ -1,30 +1,17 @@
-from celery import shared_task
-from utility.logger import appLogs 
-from django.core.mail import send_mail
-import smtplib
+from celery.decorators import task
+from celery.utils.log import get_task_logger
+from .notifications import notification
 
-app = ""
 
-def notification(subject, body, discount_code, client_name,  operator_first_name="Rosemary", dest_email="tipalov351@bomoads.com"):
-    sender_email = "emenqfox@gmail.com"
+logger = get_task_logger(__name__)
 
-    try:
-        send_mail( 
-                    "RE:" + subject,
-                    "Hi "+ client_name + ',\n\n' + 
-                    body + ',\n\n' + 
-                    "You could also use our discount code, " +   discount_code  + ", for very good discounts" + ',\n\n\n\n' + 
-                    "Best," + '\n\n' + 
-                    operator_first_name,
-                    sender_email,
-                    [dest_email],
-                    auth_user=sender_email,
-                    auth_password = config('EMAIL_PASS'),
-                    fail_silently=False
-                    )
 
-    except smtplib.SMTPException as e:
-                appLogs('Error in mail sending: ', e)
+@task(name="send_email_task")
+def send_notification_email_task(subject, body, discount_code, client_name,  operator_first_name, dest_email):
+    logger.info("Sent chat to client")
+    return notification(subject, body, discount_code, client_name,  operator_first_name, dest_email)
+
+
 
 
 
